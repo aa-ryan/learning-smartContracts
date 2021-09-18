40fdbd3e4859f72cea7766f3acea002769764112
aa-ryan <aryansinha6363@gmail.com>
2021-09-18 18:46:00 +0530

    paycore, paytransfer implemented

--- algorand/scripts/periodic_payment/payment.py
+++ algorand/scripts/periodic_payment/payment.py
@@ -1,7 +1,25 @@
-from pyteal import *
+from pyteal import Int, Bytes, Addr, Txn, And, Global
 
 # template variable
 
 tmpl_fee = Int(1000)
 tmpl_period = Int(1000)
 tmpl_dur = Int(1000)
+tmpl_x = Bytes("base64", "y9OJ5MRLCHQj8GqbikAUKMBI7hom+SOj8dlopNdNHXI=")
+tmpl_amt = Int(2000)
+tmpl_rcv = Addr("ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM")
+tmpl_timeout = Int(30000)
+
+
+# Checks the type of transaction, in this case checking for pay transactio
+# Check the fee to make sure that it is less than some reasonable amount. 
+
+periodic_pay_core = And(Txn.type_enum() == Int(1),
+                            Txn.fee() < tmpl_fee)
+
+periodic_pay_transfer = And(Txn.close_remainder_to() == Global.zero_address(),
+                                Txn.receiver() == tmpl_rcv,
+                                Txn.amount() == tmpl_amt,
+                                Txn.first_valid() % tmpl_period == Int(0),
+                                Txn.last_valid() == tmpl_dur + Txn.first_valid(),
+                                Txn.lease() == tmpl_x)
